7b1c6b136964f2f0f563438c5938c85ab0e47535
danilocorrea94 <danilo.correa@fatec.sp.gov.br>
2021-03-10 09:09:56 -0300

    categorias

--- index.php
+++ index.php
@@ -309,6 +309,23 @@ $app->get("/admin/categories/:idcategory", function($idcategory) {
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory) {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>[]
+	]);
+
+
+
+});
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -26,6 +26,8 @@ class Category extends Model{
 
 		$this->setData($result[0]);
 
+		Categorie::updateFile();
+
 	}
 
 	public function get($idcategory) {
@@ -47,6 +49,22 @@ class Category extends Model{
 		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
 			':idcategory'=>$this->getcategory()
 		]);
+
+		Categorie::updateFile();
+
+	}
+
+	public static function updateFile() {
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a hrref="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('', $html));
 
 	}
 
